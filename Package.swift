9c23f17c4b08ce701d75dfa2da48ef2f7ed65be9
Tommy van der Vorst <tommy@pixelspark.nl>
2017-06-03 15:56:22 +0200

    Use custom SQLite build (with snapshot functionality) instead of system-provided SQLite

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Catena",
     dependencies: [
-		.Package(url: "https://github.com/stephencelis/CSQLite.git", majorVersion: 0),
+		.Package(url: "https://github.com/pixelspark/sqlite.git", majorVersion: 3),
 		.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
 		.Package(url: "https://github.com/jatoben/CommandLine",  "3.0.0-pre1"),
 		.Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0),
