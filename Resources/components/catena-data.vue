c6562063e10a1ef173fdc668736039e2d24cbc27
Tommy van der Vorst <tommy@pixelspark.nl>
2017-10-08 12:19:28 +0200

    Show tables in data explorer in web client

--- Resources/components/catena-data.vue
+++ Resources/components/catena-data.vue
@@ -2,6 +2,10 @@
 	<div class="catena-data">
 		<aside>
 			<ul>
+				<li v-for="(t, idx) in tables" @click="selectTable(t)" :class="{'selected': table === t}" :key="t">
+					<i class="fa fa-table"></i> {{t}}
+				</li>
+
 				<li v-for="(q, idx) in queries" @click="select(q)" :class="{'selected': query == q}" :key="idx">
 					<a href="javascript:void(0);" style="float:right;" @click="remove(idx)"><i class="fa fa-times"></i></a>
 					<code>{{q}}</code>
@@ -27,25 +31,48 @@ module.exports = {
 	
 	data: function() {
 		return {
-			queries: ["SHOW TABLES;", "SELECT * FROM grants;", "CREATE TABLE foo(x INT);"], 
+			tables: [],
+			queries: ["CREATE TABLE foo(x INT);"], 
 			query: "SHOW TABLES;", 
+			table: null,
 			typedQuery: "SHOW TABLES;"
 		};
+	},
+
+	created: function() {
+		this.refresh();
 	},
 
 	methods: {
 		select: function(q) {
 			this.query = q;
+			this.table = null;
 			this.typedQuery = q;
+		},
+
+		selectTable: function(t) {
+			this.table = t;
+			this.typedQuery = "SELECT * FROM "+t+";";
+			this.query = this.typedQuery;
 		},
 
 		setQuery: function(e) {
 			this.typedQuery = e.target.value;
+			this.table = null;
+		},
+
+		refresh: function() {
+			var self = this;
+
+			this.agent.tables(function(err, tables) {
+				tables.sort();
+				self.tables = tables;
+			});
 		},
 
 		remove: function(idx) {
 			if(this.queries[idx] == this.query) {
-				this.query = "";
+				this.query = null;
 			}
 			this.queries.splice(idx, 1);
 		},
