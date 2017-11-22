26ec9215994dc7b4e2a5379db8ba2391f2d1486e
Tommy van der Vorst <tommy@pixelspark.nl>
2017-11-22 13:41:09 +0100

    Handle unsupported Postgres protocol commands and inform the user that these are not currently supported

--- Sources/CatenaSQL/QueryProtocol.swift
+++ Sources/CatenaSQL/QueryProtocol.swift
@@ -233,26 +233,125 @@ public final class QueryClientConnection {
 		}
 	}
 
-	private func readQuery() throws -> String? {
+	private func readQuery() throws -> Bool {
 		if let type = try self.readByte() {
 			if type == CChar(Character("Q").codePoint) {
 				// Query
 				if let len = self.readInt32(), let queryData = try self.read(length: len - UInt32(4)) {
 					let trimmed = queryData.subdata(in: 0..<(queryData.endIndex.advanced(by: -1)))
-					return String(data: trimmed, encoding: .utf8)
+					if let q = String(data: trimmed, encoding: .utf8) {
+						self.state = .querying
+						self.server?.query(q, connection: self)
+						return true
+					}
+					return false
 				}
 				else {
-					return nil
+					return false
 				}
 			}
 			else if type == CChar(Character("X").codePoint) {
 				// Exit
-				return nil
+				return false
 			}
-			return nil
+			else if type == CChar(Character("P").codePoint) {
+				/* Parse message: this should parse a statement into a prepared statement and store
+				it somewhere in a [name: statement] dictionary. If the name is omitted, the statement
+				is erased at the next Parse (with name=unnamed) or Query. A stored prepared statement
+				cannot be overwritten unless first closed (except for the unnamed one).
+
+				Byte1('P')	Identifies the message as a Parse command.
+				Int32		Length of message contents in bytes, including self.
+				String		The name of the destination prepared statement (an empty string selects
+							the unnamed prepared statement).
+				String		The query string to be parsed.
+				Int16		The number of parameter data types specified (may be zero). Note that this
+							is not an indication of the number of parameters that might appear in the
+							query string, only the number that the frontend wants to prespecify types
+							for.
+
+				Then, for each parameter, there is the following:
+
+				Int32		Specifies the object ID of the parameter data type. Placing a zero here is
+							equivalent to leaving the type unspecified. */
+				if let len = self.readInt32(), let messageData = try self.read(length: len - UInt32(4)) {
+					/*let trimmed = queryData.subdata(in: 0..<(queryData.endIndex.advanced(by: -1)))
+					return String(data: trimmed, encoding: .utf8)*/
+					Log.info("[PSQL] Client attempts to use prepared statements, which is not currently supported.")
+					Log.debug("[PSQL] Message data: \(messageData)")
+					return false
+				}
+				else {
+					return false
+				}
+			}
+			else if type == CChar(Character("B").codePoint) {
+				/* Bind message
+				Byte1('B')	Identifies the message as a Bind command.
+				Int32		Length of message contents in bytes, including self.
+				String		The name of the destination portal (an empty string selects the unnamed
+							portal).
+				String		The name of the source prepared statement (an empty string selects the
+							unnamed prepared statement).
+				Int16		The number of parameter format codes that follow (denoted C below). This
+							can be zero to indicate that there are no parameters or that the parameters
+							all use the default format (text); or one, in which case the specified
+							format code is applied to all parameters; or it can equal the actual number
+							of parameters.
+				Int16[C]	The parameter format codes. Each must presently be zero (text) or one (binary).
+				Int16		The number of parameter values that follow (possibly zero). This must match
+							the number of parameters needed by the query.
+
+				Next, the following pair of fields appear for each parameter:
+
+				Int32		The length of the parameter value, in bytes (this count does not include
+							itself). Can be zero. As a special case, -1 indicates a NULL parameter
+							value. No value bytes follow in the NULL case.
+				Byten		The value of the parameter, in the format indicated by the associated
+							format code. n is the above length.
+
+				After the last parameter, the following fields appear:
+
+				Int16		The number of result-column format codes that follow (denoted R below).
+							This can be zero to indicate that there are no result columns or that the
+							result columns should all use the default format (text); or one, in which
+							case the specified format code is applied to all result columns (if any);
+							or it can equal the actual number of result columns of the query.
+
+				Int16[R]	The result-column format codes. Each must presently be zero (text) or
+							one (binary).
+
+				Should send message BindComplete to client:
+				Byte1('2')	Identifies the message as a Bind-complete indicator.
+				Int32(4)	Length of message contents in bytes, including self.
+				*/
+				return false
+			}
+			else if type == CChar(Character("E").codePoint) {
+				/* Execute message.
+				Byte1('E')	Identifies the message as an Execute command.
+				Int32		Length of message contents in bytes, including self.
+				String		The name of the portal to execute (an empty string selects the unnamed
+							portal).
+				Int32		Maximum number of rows to return, if portal contains a query that returns
+							rows (ignored otherwise). Zero denotes "no limit". */
+				return false
+			}
+			else if type == CChar(Character("C").codePoint) {
+				/* Close message
+				Byte1('C')	Identifies the message as a Close command.
+				Int32		Length of message contents in bytes, including self.
+				Byte1		'S' to close a prepared statement; or 'P' to close a portal.
+				String		The name of the prepared statement or portal to close (an empty string
+							selects the unnamed prepared statement or portal). */
+				return false
+			}
+			else {
+				return false
+			}
 		}
 		else {
-			return nil
+			return false
 		}
 	}
 
@@ -411,9 +510,7 @@ public final class QueryClientConnection {
 					// Send 'ready for query' (Z 5 I)
 					let buf = Data(bytes: [UInt8(Character("Z").codePoint), 0, 0, 0, 5, UInt8(Character("I").codePoint)])
 					try self.socket.write(from: buf)
-					if let q = try self.readQuery() {
-						self.state = .querying
-						self.server?.query(q, connection: self)
+					if try self.readQuery() {
 						return
 					}
 					else {
